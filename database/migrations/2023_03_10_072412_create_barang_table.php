097cd3b629adeaecc11b4e1f2f547288baf0f1bc
Siti Mutamimah <sitimutamimah669@gmail.com>
2023-03-10 15:02:15 +0700

    commit pertama

---
+++ database/migrations/2023_03_10_072412_create_barang_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->id();
+            $table->longText('nama_barang');
+            $table->int('stok');
+            $table->double('harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barang');
+    }
+};
---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TUGAS MVC</title>
+</head>
+<body>
+    <p class="lead">
+        <h1><center>Apakah saya sudah berhasil mengimplementasikan konsep dari MVC ?</center></h1>
+</p>
+</body>
+</html>
